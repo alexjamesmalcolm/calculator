2352fae3eb086fd74d5ff058bce189672f385a28
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-25 16:44:15 -0500

    Created and solved test shouldReturnZeroForEmptyString

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
 public class Calculator {
-
+	public int add(String input) {
+		return 0;
+	}
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class CalculatorTest {
-	
-	
+
+	@Test
+	public void shouldReturnZeroForEmptyString() {
+		Calculator underTest = new Calculator();
+		int actual = underTest.add("");
+		Assert.assertEquals(0, actual);
+	}
 
 }
